2c3c2193ea63b858dc39f96c87345e8fa0fd57a1
Matt Henry <matth@famouscost-lm.corp.yahoo.com>
2010-03-12 10:17:51 -0800

    Add emacs congfigs (yay)

---
+++ emacs.d/.gnus
@@ -0,0 +1,16 @@
+;;;;;;;;;;;;;;
+;; gnus config
+;;;;;;;;;;;;;;
+
+(add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+                                  (nnimap-address "imap.gmail.com")
+                                  (nnimap-server-port 993)
+                                  (nnimap-stream ssl)))
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "greenideas@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-local-domain "mwhenry.com")
---
+++ emacs.d/matth.el
@@ -0,0 +1,49 @@
+;;; Custom configurations
+(global-set-key [(meta spc)] 'set-mark)
+
+;; Use command as meta
+(setq ns-command-modifier 'meta) ;carbon
+(setq mac-command-modifier 'meta) ;Aquamacs
+
+;; Define fullscreen command
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                           'fullboth)))
+(global-set-key [M-ret] 'fullscreen)
+
+;; Theme
+(color-theme-vibrant-ink)
+
+;; unindent
+(defun unindent-block()
+  (shift-region (- my-tab-width))
+  (setq deactivate-mark nil))
+
+(global-set-key [C-M-tab] 'unindent-block)
+;; init.el end
+
+;; Twitter
+(autoload 'twitter-get-friends-timeline "twitter" nil t)
+(autoload 'twitter-status-edit "twitter" nil t)
+(global-set-key "\C-xt" 'twitter-get-friends-timeline)
+(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
+
+;; Sync kill ring with OSX clipboard
+(global-set-key (kbd "C-w") 'clipboard-kill-region)
+(global-set-key (kbd "M-w") 'clipboard-kill-ring-save)
+(global-set-key (kbd "C-y") 'clipboard-yank)
+
+;; Autosave files in their own directory
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
