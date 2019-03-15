a2c7552d4c100541b7542c3983d9e0417120192e
Matt Henry <matthew.w.henry@gmail.com>
2019-03-15 15:16:55 -0400

    more emacs updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,21 +1,34 @@
+;; Configure package.el to include MELPA.
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed.
+;; Stolen from https://github.com/hrs/dotfiles/blob/master/emacs/.emacs.d/init.el
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/dotemacs.org")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(org-catch-invisible-edits (quote smart))
  '(package-selected-packages
    (quote
-    (flycheck magit org helm poly-R poly-markdown polymode ess-R-data-view ess-smart-underscore ess solarized-theme pdf-tools evil ## sml-mode)))
- '(sentence-end-double-space nil))
+    (## company-tern company use-package solarized-theme sml-mode poly-R pdf-tools org magit helm flycheck evil ess))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
+(put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
