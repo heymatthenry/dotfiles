e19babd3e3310209857d0b047cd86624f78694e9
Matt Henry <matthew.w.henry@gmail.com>
2019-03-15 15:14:37 -0400

    add new emacs configs 😈

---
+++ emacs.d/init.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(org-catch-invisible-edits (quote smart))
+ '(package-selected-packages
+   (quote
+    (flycheck magit org helm poly-R poly-markdown polymode ess-R-data-view ess-smart-underscore ess solarized-theme pdf-tools evil ## sml-mode)))
+ '(sentence-end-double-space nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(put 'upcase-region 'disabled nil)
