a16e3444c46f1cd38cf73a5e73f8a11c3d93080d
Matt Henry <hey@matthenry.fyi>
2022-08-29 09:54:33 -0400

    Fix buffer close command

--- config/nvim/lua/user/bufferline.lua
+++ config/nvim/lua/user/bufferline.lua
@@ -3,8 +3,8 @@ if not status_ok then return end
 
 bufferline.setup {
     options = {
-        close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+        close_command = "bd! %d", -- can be a string | function, see "Mouse actions"
+        right_mouse_command = "bd! %d", -- can be a string | function, see "Mouse actions"
         offsets = {{filetype = "NvimTree", text = "", padding = 1}},
         separator_style = "thin" -- | "thick" | "thin" | { 'any', 'any' },
     },
