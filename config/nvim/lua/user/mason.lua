8a63c6ff413b038a718ee149538cf112e3936475
Matt Henry <hey@matthenry.fyi>
2023-03-01 09:38:54 -0500

    The fancy thing didn't work. Configure lsp directly

--- config/nvim/lua/user/mason.lua
+++ config/nvim/lua/user/mason.lua
@@ -1,6 +1,6 @@
 local servers = {
-  "cssls", "sumneko_lua", "rust_analyzer", "eslint", "html", "jsonls",
-  "tsserver", "julials", "marksman"
+  "cssls", "lua_ls", "rust_analyzer", "eslint", "html", "jsonls", "tsserver",
+  "julials", "marksman"
 }
 
 require("mason").setup()
@@ -11,28 +11,8 @@ require("mason-lspconfig").setup {
 }
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-local opts = {}
-
--- any server-specific configs can go here
-local conf_opts = {
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        diagnostics = { globals = { 'vim' } },
-        telemetry = { enable = false }
-      }
-    }
+require 'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = { diagnostics = { globals = { 'vim' } }, telemetry = { enable = false } }
   }
 }
-
-for _, server in pairs(servers) do
-  server = vim.split(server, "@")[1]
-
-  if conf_opts["sumneko_lua"] ~= nil then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
-  end
-
-  require('lspconfig')[server].setup { capabilities = capabilities, opts }
-end
