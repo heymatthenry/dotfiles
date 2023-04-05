0269e60348a86cbc8ecfa4e58ac3a70b3eec861f
matthewwhenry <matthewwhenry@fcoh2j-d56kqlf1.attlocal.net>
2023-04-05 14:29:41 -0400

    Make mason aware of nvm node

--- config/nvim/lua/user/mason.lua
+++ config/nvim/lua/user/mason.lua
@@ -1,3 +1,18 @@
+local home_dir
+if vim.fn.isdirectory('/Users/matthewwhenry') ~= 0 then
+  home_dir = "/Users/matthewwhenry/"
+else
+  home_dir = "/Users/matt"
+end
+
+local node_bin = "/.nvm/versions/node/v19.8.1/bin"
+vim.g.node_host_prog = home_dir .. node_bin .. "/node"
+
+-- for mason.nvim
+-- prereq - install lsp server in that node/bin npm i -g typescript-language-server
+-- (handled by :Mason currently)
+vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
+
 local servers = {
   "cssls", "lua_ls", "rust_analyzer", "eslint", "html", "jsonls", "tsserver",
   "julials", "marksman"
