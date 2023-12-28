7e99f1ee078cdcd9fb2d4db8d064de99d9005084
Matt Henry <hey@matthenry.fyi>
2023-12-27 19:34:00 -0500

    Rm unnecessary comments

--- config/nvim/lua/mh/keymaps.lua
+++ config/nvim/lua/mh/keymaps.lua
@@ -1,18 +1,8 @@
--- -- telescope
--- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find files" })
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope find buffer" })
-
--- -- git
--- vim.keymap.set('n', '<leader>gg', '<Cmd>Git<CR>', { desc = "Open fugitive" })
-
-
 local wk = require("which-key")
 
 wk.register({
   f = {
-    name = "find", -- optional group name
+    name = "find", 
     f = { "<cmd>Telescope find_files<cr>", "Find File" }, 
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"},
     g = { "<cmd>Telescope live_grep<cr>", "Telescope live grep" },
