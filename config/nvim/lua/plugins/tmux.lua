22763b4bcba6da133a604d317d09a52f6d273e1e
Matt Henry <hey@matthenry.fyi>
2024-01-02 14:45:06 -0500

    Add plugin & configs for tmux splits

---
+++ config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,14 @@
+return {
+  'alexghergh/nvim-tmux-navigation',
+
+  init = function()
+    local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  end
+}
