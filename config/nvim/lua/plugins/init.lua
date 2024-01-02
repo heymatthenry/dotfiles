16efdc52a08c63a318752a96b849009b61050564
Matt Henry <hey@matthenry.fyi>
2024-01-02 16:34:38 -0500

    Add indent guides

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -7,5 +7,6 @@ return {
   'tpope/vim-endwise',
   'tpope/vim-vinegar',
 
-  'sam4llis/nvim-lua-gf' -- make `gf` work in lua files
+  'sam4llis/nvim-lua-gf', -- make `gf` work in lua files
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
