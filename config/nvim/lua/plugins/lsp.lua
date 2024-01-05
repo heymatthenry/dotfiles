a973a82f269fd0409d49da9af3d382c10dd0579b
Matt Henry <hey@matthenry.fyi>
2024-01-05 16:07:23 -0500

    Plugin: +Trouble

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -111,5 +111,14 @@ return {
         ensure_installed()
       end
     end,
-  }
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+     -- your configuration comes here
+     -- or leave it empty to use the default settings
+     -- refer to the configuration section below
+  },
+}
 }
