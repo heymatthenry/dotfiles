4355e9cdcd0645aa47163021f23a1b2d6ff5b93c
Matt Henry <hey@matthenry.fyi>
2024-01-10 15:40:43 -0500

    Plugin: +conform.nvim, +nvim-lint

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,133 +1,170 @@
 local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-local fmt_group = vim.api.nvim_create_augroup('autoformat_cmds', {clear = true})
-
-local function setup_autoformat(event)
-  vim.api.nvim_clear_autocmds({group = fmt_group, buffer = event.buf})
-
-  local id = vim.tbl_get(event, 'data', 'client_id')
-  local client = id and vim.lsp.get_client_by_id(id)
-  if client == nil then
-    return
-  end
-
-  local buf_format = function(e)
-    vim.lsp.buf.format({
-      bufnr = e.buf,
-      async = false,
-      timeout_ms = 10000,
-    })
-  end
-
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    buffer = event.buf,
-    group = fmt_group,
-    desc = 'Format current buffer',
-    callback = buf_format,
-  })
-end
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'Setup format on save',
-  callback = setup_autoformat,
-})
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function(_, opts)
-      -- server names and config settings available at:
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function(_, opts)
+			-- server names and config settings available at:
+			-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
-      -- mappings of lspconfig names to mason server names:
-      -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-      require'lspconfig'.html.setup{}
-      require'lspconfig'.eslint.setup{}
-      require'lspconfig'.pyright.setup{}
-      require'lspconfig'.rust_analyzer.setup{}
-      require'lspconfig'.tsserver.setup{}
-    end
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shfmt",
-        -- python
-        "flake8",
-        "black",
-        "pyright",
-        -- web
-        "eslint-lsp",
-        "html-lsp",
-        "json-lsp", 
-        "deno",
-        "css-lsp",
-        "prettier",
-        -- rust
-        "rust-analyzer",
-        -- julia
-        "julia-lsp",
-        --md
-        "marksman"
-      },
-    },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      mr:on("package:install:success", function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger({
-            event = "FileType",
-            buf = vim.api.nvim_get_current_buf(),
-          })
-        end, 100)
-      end)
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
-        end
-      end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
-    end,
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-     -- your configuration comes here
-     -- or leave it empty to use the default settings
-     -- refer to the configuration section below
-    }
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({})
-    end,
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-tree/nvim-web-devicons'
-    }
-  },
+			-- mappings of lspconfig names to mason server names:
+			-- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+			require("lspconfig").html.setup({})
+			require("lspconfig").eslint.setup({})
+			require("lspconfig").pyright.setup({})
+			require("lspconfig").rust_analyzer.setup({})
+			require("lspconfig").tsserver.setup({})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shfmt",
+				-- python
+				"flake8",
+				"black",
+				"pyright",
+				-- web
+				"eslint-lsp",
+				"html-lsp",
+				"json-lsp",
+				"deno",
+				"css-lsp",
+				"prettier",
+				-- rust
+				"rust-analyzer",
+				-- julia
+				"julia-lsp",
+				--md
+				"marksman",
+			},
+		},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			mr:on("package:install:success", function()
+				vim.defer_fn(function()
+					-- trigger FileType event to possibly load this newly installed LSP server
+					require("lazy.core.handler.event").trigger({
+						event = "FileType",
+						buf = vim.api.nvim_get_current_buf(),
+					})
+				end, 100)
+			end)
+			local function ensure_installed()
+				for _, tool in ipairs(opts.ensure_installed) do
+					local p = mr.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end
+			if mr.refresh then
+				mr.refresh(ensure_installed)
+			else
+				ensure_installed()
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>ll", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>lf",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { { "prettierd", "prettier" } },
+				yaml = { { "prettierd", "prettier" } },
+				rust = { "rustfmt" },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
 }
