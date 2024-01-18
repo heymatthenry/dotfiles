867d2b2e20666da8c79a2bf616db2c391d7230e2
Matt Henry <hey@matthenry.fyi>
2024-01-18 14:18:53 -0500

    Theme change

--- config/nvim/lua/plugins/themes.lua
+++ config/nvim/lua/plugins/themes.lua
@@ -1,25 +1,32 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = "moon"
-    },
-    config = function()
-      -- load the colorscheme here
-      -- vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {
+			style = "moon",
+		},
+		config = function()
+			-- load the colorscheme here
+			-- vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-  { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin-mocha]])
-    end,
-  }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			integrations = {
+				indent_blankline = {
+					enabled = true,
+					scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+					colored_indent_levels = false,
+				},
+			},
+		},
+		config = function()
+			vim.cmd([[colorscheme catppuccin-mocha]])
+		end,
+	},
 }
