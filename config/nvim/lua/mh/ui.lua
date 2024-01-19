abb8eab9991472e4d8f26195d37c8718a9f86c44
Matt Henry <hey@matthenry.fyi>
2024-01-18 20:39:13 -0500

    Fancy md stuff needs conceallevel set

--- config/nvim/lua/mh/ui.lua
+++ config/nvim/lua/mh/ui.lua
@@ -1,11 +1,12 @@
 vim.opt.cursorline = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.termguicolors = true
+vim.opt.conceallevel = 2
 
-require('lualine').setup {
-  options = {
-    theme = 'catppuccin-mocha',
-    section_separators = '',
-    component_separators = ''
-  }
-}
+require("lualine").setup({
+	options = {
+		theme = "catppuccin-mocha",
+		section_separators = "",
+		component_separators = "",
+	},
+})
