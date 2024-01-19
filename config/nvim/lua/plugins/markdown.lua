98e739e229ac1e6036ad5b9c4b165dc6af7d0950
Matt Henry <hey@matthenry.fyi>
2024-01-18 20:37:20 -0500

    Plugin: +obsidian, -vimwiki

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,63 +1,71 @@
 return {
-  { 
-    'vimwiki/vimwiki', 
-    init = function()
-      vim.g.vimwiki_list = {
-        {
-          path = "~/vimwiki",
-          path_html= "~/vimwiki/public_html",
-          auto_tags = 1,
-          auto_diary_index = 1,
-          syntax = 'markdown',
-          ext = '.md'
-        },
-      }
-      vim.g.vimwiki_markdown_link_ext = 1
-      vim.g.vimwiki_stripsym = ' '
-      vim.g.vimwiki_global_ext = 0
-    end
-  },
-  {
-    "lukas-reineke/headlines.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-    init = function()
-      require("headlines").setup {
-        vimwiki = { -- this might be ['vimwiki.markdown'] depending on your vimwiki configuration
-          treesitter_language = "markdown",
-          query = vim.treesitter.query.parse(
-              "markdown",
-              [[
-                  (atx_heading [
-                      (atx_h1_marker)
-                      (atx_h2_marker)
-                      (atx_h3_marker)
-                      (atx_h4_marker)
-                      (atx_h5_marker)
-                      (atx_h6_marker)
-                  ] @headline)
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
 
-                  (thematic_break) @dash
-
-                  (fenced_code_block) @codeblock
-
-                  (block_quote_marker) @quote
-                  (block_quote (paragraph (inline (block_continuation) @quote)))
-                  (block_quote (paragraph (block_continuation) @quote))
-                  (block_quote (block_continuation) @quote)
-              ]]
-          ),
-          headline_highlights = { "Headline" },
-          codeblock_highlight = "CodeBlock",
-          dash_highlight = "Dash",
-          dash_string = "-",
-          quote_highlight = "Quote",
-          quote_string = "┃",
-          fat_headlines = true,
-          fat_headline_upper_string = "▃",
-          fat_headline_lower_string = "🬂",
-        }
-      }
-    end
-  },
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/Documents/Notes",
+				},
+			},
+			daily_notes = {
+				folder = "Daily Notes",
+				date_format = "%Y-%m-%d",
+			},
+			mappings = {
+				-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+				["gf"] = {
+					action = function()
+						return require("obsidian").util.gf_passthrough()
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+			},
+			finder = "telescope.nvim",
+			ui = {
+				enable = true, -- set to false to disable all additional syntax features
+				update_debounce = 200, -- update delay after a text change (in milliseconds)
+				-- Define how various check-boxes are displayed
+				checkboxes = {
+					-- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+					[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+					["x"] = { char = "", hl_group = "ObsidianDone" },
+					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+					["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				},
+				-- Use bullet marks for non-checkbox lists.
+				bullets = { char = "•", hl_group = "ObsidianBullet" },
+				external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+				-- Replace the above with this if you don't have a patched font:
+				-- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+				reference_text = { hl_group = "ObsidianRefText" },
+				highlight_text = { hl_group = "ObsidianHighlightText" },
+				tags = { hl_group = "ObsidianTag" },
+				hl_groups = {
+					-- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+					ObsidianTodo = { bold = true, fg = "#f78c6c" },
+					ObsidianDone = { bold = true, fg = "#89ddff" },
+					ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+					ObsidianTilde = { bold = true, fg = "#ff5370" },
+					ObsidianBullet = { bold = true, fg = "#89ddff" },
+					ObsidianRefText = { underline = true, fg = "#c792ea" },
+					ObsidianExtLinkIcon = { fg = "#c792ea" },
+					ObsidianTag = { italic = true, fg = "#89ddff" },
+					ObsidianHighlightText = { bg = "#75662e" },
+				},
+			},
+		},
+	},
+	{
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true,
+	},
 }
