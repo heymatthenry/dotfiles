f858f1fa3024766c52735904e47c70356daed747
Matt Henry <hey@matthenry.fyi>
2024-01-21 17:17:13 -0500

    Plugins: +octo.nvim

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,7 +1,20 @@
 return {
-  'lewis6991/gitsigns.nvim',
+	{
+		"lewis6991/gitsigns.nvim",
 
-  config = function()
-    require('gitsigns').setup()
-  end
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
 }
