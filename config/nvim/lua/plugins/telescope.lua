3377d1e5b990704c2286c8ec2506acd363b82286
Matt Henry <hey@matthenry.fyi>
2024-01-28 15:50:42 -0500

    use centered layout for telescope picker

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,17 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						theme = "dropdown",
+					},
+				},
+			})
+		end,
+	},
 }
