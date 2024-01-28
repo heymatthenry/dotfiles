70eaeb7aef82aa8caf7edfe2f9cc36f7289e24fe
Matt Henry <hey@matthenry.fyi>
2024-01-28 12:02:53 -0500

    rm duplicated diagnostic setup

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,3 @@
--- Configure diagnostic display
-
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 return {
 	{
 		"neovim/nvim-lspconfig",
