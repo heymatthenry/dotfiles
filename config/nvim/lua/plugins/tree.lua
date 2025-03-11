be9a8b5aaed90abc3173cd9d21bbb2cb65bc0580
Matt Henry <matt.henry@gsa.gov>
2025-03-11 09:39:17 -0400

    Update plugins; remove image.nvim
    Image never worked for me and it was the only
    plugin that required luarocks, which was giving
    `checkhealth` warnings.

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -1,28 +1,27 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
 
-    config = function ()
-      require("neo-tree").setup({
-        close_if_last_window = false,
-        filesystem = {
-          hijack_netrw_behavior = "disabled",
-        },
-        window = {
-          position = "right",
-          width = 30,
-          mappings = {
-            ["ga"] = "git_add_file",
-          }
-        }
-      })
-    end
-  }
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = false,
+				filesystem = {
+					hijack_netrw_behavior = "disabled",
+				},
+				window = {
+					position = "right",
+					width = 30,
+					mappings = {
+						["ga"] = "git_add_file",
+					},
+				},
+			})
+		end,
+	},
 }
